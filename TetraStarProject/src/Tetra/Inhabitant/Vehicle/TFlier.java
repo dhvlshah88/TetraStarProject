a327b880010f17785379acd894ab3b89be4f429d
Dhaval Shah <dhvlshah88@gmail.com>
2012-08-23 03:06:59 -0700

    In this class, I implemented single parameter constructor.

---
+++ TetraStarProject/src/Tetra/Inhabitant/Vehicle/TFlier.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package Tetra.Inhabitant.Vehicle;
+
+import Tetra.Position;
+
+/**
+ * @author Dhaval
+ *
+ */
+public class TFlier extends Vehicle {
+
+	public TFlier(){
+		
+	}
+	
+	public TFlier(String tflierId){
+		super(tflierId);
+	}
+	
+	/* (non-Javadoc)
+	 * @see Tetra.Inhabitant.Vehicle#moveToPosition()
+	 */
+	@Override
+	public Position moveToPosition() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
