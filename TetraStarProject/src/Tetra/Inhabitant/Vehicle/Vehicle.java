b745c54c4d06f014612eee60fa53a6db632f450f
Dhaval Shah <dhvlshah88@gmail.com>
2012-08-26 08:05:45 -0700

    Re-factor getCurrentPosition method to getPosition method. And wrote description to each method.

--- TetraStarProject/src/Tetra/Inhabitant/Vehicle/Vehicle.java
+++ TetraStarProject/src/Tetra/Inhabitant/Vehicle/Vehicle.java
@@ -15,36 +15,73 @@ public abstract class Vehicle {
 		
 	}
 	
+	/**
+	 * Parameterized constructor.
+	 * @param vehicleId
+	 */
 	public Vehicle(String vehicleId){
 		this.vehicleId = vehicleId;
 	}
 	
+	/**
+	 * This method sets vehicle id.
+	 * @param vehicleId
+	 */
 	public void setId(String vehicleId){
 		this.vehicleId = vehicleId;
 	}
 	
+	/**
+	 *  This method gets vehicle id.
+	 * @return {@link String}
+	 */
 	public String getId(){
 		return vehicleId;
 	}
 	
-	public void setCurrentPosition(Position currentPosition){
+	/**
+	 *  This method sets vehicle position on grid.
+	 * @param currentPosition
+	 */
+	public void setPosition(Position currentPosition){
 		this.currentPosition = currentPosition;
 	}
 	
-	public Position getCurrentPosition(){
+	/**
+	 * This method gets vehicle position on grid.
+	 * @return {@link Position}
+	 */
+	public Position getPosition(){
 		return currentPosition;
 	}
 
+	/**
+	 * This method sets vehicle's possession value.
+	 * @param vehiclePossessed
+	 */
 	public void setPossession(boolean vehiclePossessed){
 		this.vehiclePossessed = vehiclePossessed;
 	}
 	
+	/**
+	 * This method gets vehicle's possession value.
+	 * @return {@link Boolean}
+	 */
 	public boolean isPossessed(){
 		return vehiclePossessed;
 	}
 	
+	/**
+	 * Abstract method.
+	 * @return {@link Position}
+	 */
 	public abstract Position moveToPosition();
 	
+	/**
+	 * This overrode method compares this object's id with given vehicle object's id. Returns TRUE if compares successfully 
+	 * else FALSE. 
+	 * @return {@link Boolean}
+	 */
 	@Override
 	public boolean equals(Object obj) {
 		if(!(obj instanceof Vehicle)){
