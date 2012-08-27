fc9fbd663cc59940e8d4c0eda2b982ebec3c9c3c
Dhaval Shah <dhvlshah88@gmail.com>
2012-08-27 12:07:10 -0700

    This is a common interface for all the tetra entities which has getPosition method.

---
+++ TetraStarProject/src/Tetra/ILocatable.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package Tetra;
+
+/**
+ * @author Dhaval
+ *
+ */
+public interface ILocatable {
+	
+	/**
+	 * This method return the <code>Position</code> object.
+	 * @return {@link Position}
+	 */
+	public Position getPosition();
+
+}
