2bb33c6ae4fc596d1fb76dd723edee321e458e3c
Dhaval Shah <dhvlshah88@gmail.com>
2012-08-28 13:26:02 -0700

    This class maintains collection of all the Position object created i.e. total rows * total columns.

--- TetraStarProject/src/Tetra/TFace.java
+++ TetraStarProject/src/Tetra/TFace.java
@@ -3,60 +3,88 @@
  */
 package Tetra;
 
-import javax.swing.BorderFactory;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.GridLayout;
+import java.util.HashMap;
 
 /**
  * @author Dhaval
  *
  */
 public class TFace {
-	
-	JFrame tetraFrame = new JFrame();
-	Dimension frameDimension;
-	Dimension panelDimension;
 
-	
 	/**
 	 * 
 	 */
-	public TFace(int rows, int columns) {
-		// TODO Auto-generated constructor stub
-		
-		frameDimension = new Dimension(1000, 700);
-		tetraFrame.setPreferredSize(frameDimension);
-		tetraFrame.setLayout(new GridLayout(rows, columns));
-		panelDimension = new Dimension(frameDimension.width/rows, frameDimension.height/columns);
-		JPanel[][] arrPanel = new JPanel[rows][columns];
-		
-		for(int i = 0; i < rows; i++){
-			for(int j = 0; j < columns; j++){
-			arrPanel[i][j] = new JPanel();
-			arrPanel[i][j].setPreferredSize(panelDimension);
-			
-			//if ((i + j) % 2 == 0) {
-	                arrPanel[i][j].setBackground(Color.white);
-	        /* } 
-			 else {
-				 arrPanel[i][j].setBackground(Color.white);
-	         }*/
-			
-			arrPanel[i][j].setBorder(BorderFactory.createLineBorder(Color.black,1));
-			tetraFrame.add(arrPanel[i][j]);
-			
-		  }
+	
+	private HashMap<String, Position> positionCollection = null;
+	private Position position = null;
+	private int totalRows, totalColumns = 0;
+	
+	/**
+	 * Parameterized Constructor
+	 * @param totalRows <i>Total number of rows</i>
+	 * @param totalColumns <i>Total number of columns</i>
+	 */
+	public TFace(int totalRows, int totalColumns) {
+		this.totalRows = totalRows;
+		this.totalColumns = totalColumns;
+		buildHashMap(totalRows, totalColumns);
+	}
+	
+	/**
+	 * This methods builds hash map of <code>Position</code> instance by creating them. A total of rows * columns 
+	 * <code>Position</code> instance are created and stored in hash map.
+	 * @param totalRows <i>Total number of rows</i>
+	 * @param totalColumns <i>Total number of columns</i>
+	 */
+	public void buildHashMap(int totalRows, int totalColumns){
+		positionCollection = new HashMap<String, Position>();
+		if(!positionCollection.isEmpty()){
+			return;
 		}
-		tetraFrame.pack();
-		tetraFrame.setVisible(true);
+		
+		for(int rowNo = 0; rowNo < totalRows; rowNo++ ){
+			for(int columnNo = 0; columnNo < totalColumns; columnNo++){
+				position = createPosition(rowNo, columnNo);
+				positionCollection.put(position.toString(), position);
+			}
+		}
 	}
-
-	public TFace(){
-	this(6,6);	
+	
+	/**
+	 * This method is factory method for creating <code>Position</code> objects.
+	 * @param rowNo <i>row number</i>
+	 * @param columnNo <i>column number</i>
+	 * @return {@link Position} object.
+	 */
+	public Position createPosition(int rowNo, int columnNo){
+		return new Position(rowNo, columnNo);
 	}
-
+	
+	/**
+	 * This method returns the hash map build by <code>buildHashMap(int, int)</code>.
+	 * @return {@link HashMap} of <code>Position</code> objects.
+	 */
+	public HashMap<String, Position> getPositionCollection(){
+		return positionCollection;
+	}
+	
+	/**
+	 * This method gets total number of rows in grid.
+	 * @return {@link Integer} <code>totalRows</code>
+	 */
+	public int getTotalRows(){
+		return totalRows;
+	}
+	
+	/**
+	 * This method gets total number of columns to grid.
+	 * @return {@link Integer} <code>totalColumns</code>
+	 */
+	public int getTotalColumns(){
+		return totalColumns;
+	}
+	
+	public Position getPosition(int rowNo, int columnNo){
+		return positionCollection.get(Integer.toString(rowNo).concat(Integer.toString(columnNo)));
+	}
 }
