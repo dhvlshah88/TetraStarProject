9196684d1010a4e2e5bca00a345cafcb40bb5b8a
Dhaval Shah <dhvlshah88@gmail.com>
2012-08-28 13:06:36 -0700

    Added a new class Logger. This class is use to log all the details on GUI instead of text file.

---
+++ TetraStarProject/src/Tetra/Logger.java
@@ -0,0 +1,23 @@
+package Tetra;
+
+import TetraGUI.*;
+
+/**
+ * 
+ * @author Pranav
+ *
+ */
+
+public class Logger {
+
+	/**
+	 * This static method calls <code>DisplaySteps()</code> method of <code>TetraStarGUI</code> class to print steps
+	 * executed on GUI's text area component.
+	 * @param steps 
+	 */
+	public static void DisplaySteps(String steps){
+		TetraStarGUI.DisplaySteps(steps);
+	}
+	
+}
+
