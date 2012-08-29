ad4e1795e0bca6a4866a3f56141b03e524b03929
Dhaval Shah <dhvlshah88@gmail.com>
2012-08-29 06:55:32 -0700

    Added TRoverView. Added new images. Removed THeroWithTFlier.

---
+++ TetraStarProject/src/TetraGUI/BaseView.java
@@ -0,0 +1,47 @@
+package TetraGUI;
+
+import javax.swing.JPanel;
+
+import Tetra.Base.*;
+
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.BorderLayout;
+import java.awt.Graphics;
+import java.awt.Image;
+
+public class BaseView extends JPanel {
+	Image image;
+	Dimension cellDimension = null;
+	Base base = null;
+
+	/**
+	 * Create the panel.
+	 */
+	public BaseView(Dimension cellDimension , Base base) {
+		this.base  = base;
+		this.cellDimension = cellDimension;
+
+		try
+		{
+			image = javax.imageio.ImageIO.read(new java.net.URL(BaseView.class.getResource(base.getImageFilePath()), "MapBaseImage.jpg"));
+		}
+		catch (Exception e) {
+			System.out.println("Exception: " + e);
+		}
+		setPreferredSize(cellDimension);
+		setLayout(new BorderLayout(0, 0));
+		setVisible(true);
+		setBackground(new Color(150, 93, 82));
+
+	}
+	
+	protected void paintComponent(Graphics g)
+	{
+		super.paintComponent(g); 
+		if (image != null){
+			g.drawImage(image, 0, 0, (int) cellDimension.getWidth(), (int)cellDimension.getHeight(),this);
+		}
+	}
+}
---
+++ TetraStarProject/src/TetraGUI/TRoverView.java
@@ -0,0 +1,50 @@
+package TetraGUI;
+
+import javax.swing.JPanel;
+import Tetra.Inhabitant.TRovers;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.BorderLayout;
+import java.awt.Image;
+
+public class TRoverView extends JPanel {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -9154998090722585010L;
+	Image image;
+	TRovers tRovers = null;
+	Dimension cellDimension = null;
+
+	public TRoverView(Dimension cellDimension , TRovers tRovers) {
+		this.tRovers = tRovers;
+		this.cellDimension = cellDimension;
+
+		try
+		{
+			image = javax.imageio.ImageIO.read(new java.net.URL(TRoverView.class.getResource(tRovers.getImageFilePath()), ""));
+		}
+		catch (Exception e) { 
+			/*handled in paintComponent()*/ 
+		}
+
+		setPreferredSize(cellDimension);
+		setLayout(new BorderLayout(0, 0));
+		setVisible(true);
+		setBackground(Color.black);
+		setOpaque(false);
+		repaint();
+	}
+
+	@Override
+	protected void paintComponent(Graphics g)
+	{
+		super.paintComponent(g); 
+		if (image != null){
+			g.drawImage(image, 0, 0, (int)cellDimension.getWidth(), (int)cellDimension.getHeight(), this);
+		}
+	}
+
+}
