f6423e858c9a72605b3ab7730b7554378c57310f
Dhaval Shah <dhvlshah88@gmail.com>
2012-08-29 22:40:43 -0700

    Scenario and ScenarioOne class

--- TetraStarProject/src/Tetra/Scenario.java
+++ TetraStarProject/src/Tetra/Scenario.java
@@ -7,10 +7,16 @@ import java.awt.event.MouseAdapter;
 
 import Tetra.Base.Base;
 import Tetra.Base.BaseFactory;
+import Tetra.Base.MapBase;
+import Tetra.Inhabitant.THero;
 import Tetra.Inhabitant.TRoverFactory;
 import Tetra.Inhabitant.TRovers;
+import Tetra.Inhabitant.TVader;
+import Tetra.Map.Map;
+import Tetra.Map.MapFactory;
 import TetraGUI.BaseView;
 import TetraGUI.TRoverView;
+import TetraGUI.TetraGUIManager;
 import TetraGUI.TetraStarGUI;
 
 /**
@@ -21,95 +27,200 @@ public class Scenario extends MouseAdapter {
 
 	private TetraStarGUI gui = null;
 	private TFace tface = null;
-
+	
+	private TRovers trover1 = null;
+	private TRovers trover2 = null;
+	private TVader tvader = null; 
+	private THero thero = null;
+	private Map map1 = null;
+	private Map map2 = null;
+	private Map map3 = null;
+	private MapBase mapbase1 = null;
+	private MapBase mapbase2 = null;
+	
+	private TInhabitantCollection inhabitantColl = null;
+	private TBaseCollection baseColl = null;
+	private VehicleCollection vehicleColl = null;
+	
+	private TetraGUIManager guiMngr = null;
+	
+	private TRoverFactory roverFactory = null;
+	private MapFactory mapFactory = null;
+	private TRoverView firstroverView = null;
+	private TRoverView secondroverView = null;
+	
 	public Scenario(TetraStarGUI gui, TFace tface){
 		this.gui = gui;
 		this.tface = tface;
+		
+		
+		roverFactory = new TRoverFactory();
+		mapFactory = new MapFactory();
+		
+		inhabitantColl = new TInhabitantCollection();
+		baseColl = new TBaseCollection();
+		vehicleColl = new VehicleCollection();
+		
+		
+		guiMngr = new TetraGUIManager(gui, tface, inhabitantColl);
+		
 
 		gui.addMouseListener(this);
 		
-		addTRovers(gui, tface);
 
-		addMapBase(gui, tface);
+		addMapBase();
 
-		addVaderBase(gui, tface);
+		addVaderBase();
 
-		addVader(gui, tface);
+		addRiver();
 
-		addRiver(gui, tface);
+		addHeroBase();
 
-		addHeroBase(gui, tface);
+		addVader();
 
-		addHero(gui, tface);
+		addTRovers();
+
+		addHero();
+		
+		
 	}
 
 
-	private static void addRiver(TetraStarGUI gui, TFace tface){
+	
+	private void addRiver(){
 		Base firstriver = BaseFactory.createBase("River", tface.getPosition(2,3));
 		BaseView firstriverView = new BaseView(gui.getCellDimension(), firstriver);
-		gui.drawLocatableObj(firstriver.getPosition(), firstriverView);
+		guiMngr.drawGrid(firstriver.getPosition(), firstriverView);
 
 		Base secondriver = BaseFactory.createBase("River", tface.getPosition(3,2));
 		BaseView secondriverView = new BaseView(gui.getCellDimension(), secondriver);
-		gui.drawLocatableObj(secondriver.getPosition(), secondriverView);
+		guiMngr.drawGrid(secondriver.getPosition(), secondriverView);
 
 		Base thirdriver = BaseFactory.createBase("River", tface.getPosition(4,3));
 		BaseView thirdriverView = new BaseView(gui.getCellDimension(), thirdriver);
-		gui.drawLocatableObj(thirdriver.getPosition(), thirdriverView);
+		guiMngr.drawGrid(thirdriver.getPosition(), thirdriverView);
 
 		Base fourthriver = BaseFactory.createBase("River", tface.getPosition(3,4));
 		BaseView fourthriverView = new BaseView(gui.getCellDimension(), fourthriver);
-		gui.drawLocatableObj(fourthriver.getPosition(), fourthriverView);
+		guiMngr.drawGrid(fourthriver.getPosition(), fourthriverView);
+		
+		baseColl.addLocatableObject(firstriver);
+		baseColl.addLocatableObject(secondriver);
+		baseColl.addLocatableObject(thirdriver);
+		baseColl.addLocatableObject(fourthriver);
+		
 	}
 
-	private static void addVaderBase(TetraStarGUI gui, TFace tface){
+	
+	private void addVaderBase(){
 		Base vaderbase = BaseFactory.createBase("TVaderBase", tface.getPosition(3,3));
 		BaseView vaderBaseView = new BaseView(gui.getCellDimension(), vaderbase);
-		gui.drawLocatableObj(vaderbase.getPosition(), vaderBaseView);
+		guiMngr.drawGrid(vaderbase.getPosition(), vaderBaseView);
+		
+		baseColl.addLocatableObject(vaderbase);
 	}
 
-	private static void addHeroBase(TetraStarGUI gui, TFace tface){
+	
+	private void addHeroBase(){
 		Base herobase = BaseFactory.createBase("THeroBase", tface.getPosition(0,0));
 		BaseView heroBaseView = new BaseView(gui.getCellDimension(), herobase);
-		gui.drawLocatableObj(herobase.getPosition(), heroBaseView);
-	}
+		guiMngr.drawGrid(herobase.getPosition(), heroBaseView);
+		
+		baseColl.addLocatableObject(herobase);
+		}
 
-	private static void addMapBase(TetraStarGUI gui, TFace tface){
-		Base mapbase1 = BaseFactory.createBase("MapBase", tface.getPosition(1,6));
+	
+	
+	private void addMapBase(){
+		mapbase1 =(MapBase) BaseFactory.createBase("MapBase", tface.getPosition(1,6));
+		mapbase1.setTface(tface);
 		BaseView mapBaseView1 = new BaseView(gui.getCellDimension(), mapbase1);
-		gui.drawLocatableObj(mapbase1.getPosition(), mapBaseView1);
+		guiMngr.drawGrid(mapbase1.getPosition(), mapBaseView1);
 
-		Base mapbase2 = BaseFactory.createBase("MapBase", tface.getPosition(7,4));
+		map1 = mapFactory.createMap("StarAtlas", mapbase1.getPosition(), "M1");
+		map1.setItemCount(0);
+		
+		map3 = mapFactory.createMap("StarMap", mapbase1.getPosition(), "M3");
+		map3.setDirection("Inside StarAtlas.");
+		map3.setItemCount(0);
+		map1.addStarMap(map3);
+		mapbase1.setMap(map1);
+		
+		mapbase2 = (MapBase) BaseFactory.createBase("MapBase", tface.getPosition(7,4));
 		BaseView mapBaseView2 = new BaseView(gui.getCellDimension(), mapbase2);
-		gui.drawLocatableObj(mapbase2.getPosition(), mapBaseView2);
+		guiMngr.drawGrid(mapbase2.getPosition(), mapBaseView2);
+		
+		map2 = mapFactory.createMap("StarMap", mapbase2.getPosition(), "M2");
+		map2.setDirection("Rovers displayed map!!");
+		map2.setItemCount(0);
+		mapbase2.setMap(map2);
+		
+		baseColl.addLocatableObject(mapbase1);
+		baseColl.addLocatableObject(mapbase2);
+		
 	}
 
-	private static void addTRovers(TetraStarGUI gui, TFace tface){
-		TRoverFactory roverFactory = new TRoverFactory();
+	
+	private void addTRovers(){
+		trover1 = roverFactory.createTRover("TRover", tface.getPosition(5,6));
+		firstroverView = (TRoverView) guiMngr.generateViews(trover1); 
+		trover1.setName("BumbleBee1");
+		trover1.setTetId("TR1");
+		trover1.setGender("M");
+		guiMngr.drawGrid(trover1.getPosition(), firstroverView);
+	
+		trover2 = roverFactory.createTRover("TRover", tface.getPosition(7,0));
+		secondroverView = (TRoverView) guiMngr.generateViews(trover2);
+		trover1.setName("BumbleBee2");
+		trover1.setTetId("TR2");
+		trover1.setGender("M");
+		guiMngr.drawGrid(trover2.getPosition(), secondroverView);
 
-		TRovers trover1 = roverFactory.createTRover("TRover", tface.getPosition(5,6));
-		TRoverView firstroverView = new TRoverView(gui.getCellDimension(), trover1);
-		gui.drawLocatableObj(trover1.getPosition(), firstroverView);
-
-		TRovers trover2 = roverFactory.createTRover("TRover", tface.getPosition(7,0));
-		TRoverView secondroverView = new TRoverView(gui.getCellDimension(), trover2);
-		gui.drawLocatableObj(trover2.getPosition(), secondroverView);
-
+		inhabitantColl.addLocatableObject(trover1);
+		inhabitantColl.addLocatableObject(trover2);
+		trover1.setInhabitantColl(inhabitantColl);
+		trover1.setBaseColl(baseColl);
+		trover1.setVehicleColl(vehicleColl);
+		trover2.setInhabitantColl(inhabitantColl);
+		trover2.setBaseColl(baseColl);
+		trover2.setVehicleColl(vehicleColl);
 	}
 
-	private static void addVader(TetraStarGUI gui, TFace tface){
-		TRoverFactory roverFactory = new TRoverFactory();
-		TRovers tvader = roverFactory.createTRover("TVader", tface.getPosition(3,3));
+	private void addVader(){
+		tvader = (TVader) roverFactory.createTRover("TVader", tface.getPosition(3,3));
 		TRoverView vaderView = new TRoverView(gui.getCellDimension(), tvader);
-		gui.drawLocatableObj(tvader.getPosition(), vaderView);
+		gui.initializeGrid(tvader.getPosition(), vaderView);
+		
+		inhabitantColl.addLocatableObject(tvader);
+		tvader.setInhabitantColl(inhabitantColl);
+		tvader.setBaseColl(baseColl);
+		tvader.setVehicleColl(vehicleColl);
 	}
 
-	private static void addHero(TetraStarGUI gui, TFace tface){
-		TRoverFactory roverFactory = new TRoverFactory();
-		TRovers thero = roverFactory.createTRover("THero", tface.getPosition(3,7));
+	private void addHero(){
+		thero = (THero) roverFactory.createTRover("THero", tface.getPosition(3,7));
 		TRoverView heroView = new TRoverView(gui.getCellDimension(), thero);
-		gui.drawLocatableObj(thero.getPosition(), heroView);
+		gui.initializeGrid(thero.getPosition(), heroView);
+		
+		inhabitantColl.addLocatableObject(thero);
+		thero.setInhabitantColl(inhabitantColl);
+		thero.setBaseColl(baseColl);
+		thero.setVehicleColl(vehicleColl);
+	}
+	
+	private void addVehicle(){
+		
+	}
+	
+	private void addMap(){
+		
 	}
 
+	public TRovers getTrover1(){
+		return trover1;
+	}
 
+	public TRovers getTrover2(){
+		return trover2;
+	}
 }
---
+++ TetraStarProject/src/Tetra/ScenarioOne.java
@@ -0,0 +1,82 @@
+/**
+ * 
+ */
+package Tetra;
+
+import java.awt.event.MouseEvent;
+
+import Tetra.Inhabitant.TRovers;
+import TetraGUI.TetraStarGUI;
+
+/**
+ * @author pranav
+ *
+ */
+public class ScenarioOne extends Scenario {
+
+	private Position nextPosition;
+	private	TFace tface = null;
+	private static int steps = 1;
+	TRovers tRover ;
+
+	/**
+	 * @param gui
+	 * @param tface
+	 */
+	public ScenarioOne(TetraStarGUI gui, TFace tface) {
+		super(gui, tface);
+		this.tface = tface;
+		tRover = super.getTrover1();
+	}
+
+	public void mouseClicked(MouseEvent e) {
+
+		switch(steps){
+
+		case 1:
+			nextPosition = tface.getPosition(5,5);
+			tRover.moveToPosition(nextPosition);
+			steps++;
+			break;
+		case 2:
+			nextPosition = tface.getPosition(5,4);
+			tRover.moveToPosition(nextPosition);
+			steps++;
+			break;
+		case 3:
+			nextPosition = tface.getPosition(5,3);
+			tRover.moveToPosition(nextPosition);
+			steps++;
+			break;
+		case 4:
+			nextPosition = tface.getPosition(5,2);
+			tRover.moveToPosition(nextPosition);
+			steps++;
+			break;
+		case 5:
+			nextPosition = tface.getPosition(6,2);
+			tRover.moveToPosition(nextPosition);
+			steps++;
+			break;
+		case 6:
+			nextPosition = tface.getPosition(7,2);
+			tRover.moveToPosition(nextPosition);
+			steps++;
+			break;
+		case 7:
+			nextPosition = tface.getPosition(7,3);
+			tRover.moveToPosition(nextPosition);
+			steps++;
+			break;
+		case 8:
+			nextPosition = tface.getPosition(7,4);
+			tRover.moveToPosition(nextPosition);
+			steps++;
+			break;
+		}
+	}
+
+
+
+
+}
